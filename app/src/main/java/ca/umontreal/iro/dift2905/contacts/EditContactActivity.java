b231cc9758304f45b9e8baf4ce8477871931021c
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-04-22 17:30:21 -0400

    Generating MainActivity and EditContactActivity

---
+++ app/src/main/java/ca/umontreal/iro/dift2905/contacts/EditContactActivity.java
@@ -0,0 +1,14 @@
+package ca.umontreal.iro.dift2905.contacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_contact);
+    }
+}
