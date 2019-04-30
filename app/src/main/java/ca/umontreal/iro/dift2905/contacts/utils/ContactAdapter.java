a40cbc4d01073539a9155cbfc8a0a7d1295d50e5
debouvie <47230753+debouvie@users.noreply.github.com>
2019-04-30 11:35:51 -0400

    Update ContactAdapter.java

--- app/src/main/java/ca/umontreal/iro/dift2905/contacts/utils/ContactAdapter.java
+++ app/src/main/java/ca/umontreal/iro/dift2905/contacts/utils/ContactAdapter.java
@@ -1,6 +1,7 @@
 package ca.umontreal.iro.dift2905.contacts.utils;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 import ca.umontreal.iro.dift2905.contacts.Contact;
 import ca.umontreal.iro.dift2905.contacts.DBHelper;
+import ca.umontreal.iro.dift2905.contacts.EditContactActivity;
 import ca.umontreal.iro.dift2905.contacts.R;
 
 import static android.view.LayoutInflater.from;
@@ -22,9 +24,11 @@ import static android.view.LayoutInflater.from;
  */
 public class ContactAdapter extends Adapter<ContactViewHolder> {
     private List<Contact> mContacts;
+    private Context mContext;
 
-    public ContactAdapter(List<Contact> contacts) {
+    public ContactAdapter(List<Contact> contacts, Context context) {
         mContacts = contacts;
+        mContext = context;
     }
 
     @NonNull
@@ -36,7 +40,7 @@ public class ContactAdapter extends Adapter<ContactViewHolder> {
 
     /**
      * Méthode qui met à jour le contenu du holder
-     * avec les initials et le nom d'un contact à une position donnée
+     * et traite les actions sur un contact à une position donnée
      *
      * @param holder
      * @param position du contact
@@ -47,6 +51,17 @@ public class ContactAdapter extends Adapter<ContactViewHolder> {
 
         holder.initialsTextView.setText(contact.getInitials());
         holder.nameTextView.setText(contact.getFullName());
+        
+        /* Démarre l'activité d'édition de contact lors d'un long clic sur un contact */
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent =new Intent(mContext, EditContactActivity.class);
+                intent.putExtra("contact", contact);
+                mContext.startActivity(intent);
+                return false;
+            }
+        });
     }
 
     /**
